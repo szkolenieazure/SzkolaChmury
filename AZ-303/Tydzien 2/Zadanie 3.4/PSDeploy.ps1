0ffea72c8377a33eae88eb7496f79c0400245480
szkolenieazure <64008291+szkolenieazure@users.noreply.github.com>
2021-03-20 19:09:55 +0100

    Create PSDeploy.ps1

---
+++ AZ-303/Tydzien 2/Zadanie 3.4/PSDeploy.ps1
@@ -0,0 +1,2 @@
+New-AzResourceGroup -Name Contoso-RG -Location "westeurope"
+New-AzResourceGroupDeployment -ResourceGroupName "Contoso-RG" -TemplateUri "https://raw.githubusercontent.com/szkolenieazure/SzkolaChmury/main/AZ-303/Tydzien%202/Zadanie%203.4/main.json"
